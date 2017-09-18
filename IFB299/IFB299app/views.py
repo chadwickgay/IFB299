0f0f8ea6b5c8b8253dc76d808123f3c224a09f62
teagan23 <30581484+teagan23@users.noreply.github.com>
2017-09-18 23:14:12 +1000

    Update views.py

--- IFB299/IFB299app/views.py
+++ IFB299/IFB299app/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
 
 # Create your views here.
 
