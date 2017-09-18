9502142efe48c6e323204f62c6ba6687f2c64f1f
teagan23 <30581484+teagan23@users.noreply.github.com>
2017-09-18 23:20:23 +1000

    Update views.py

--- IFB299/IFB299app/views.py
+++ IFB299/IFB299app/views.py
@@ -23,3 +23,36 @@ def register(request):
 def dashboard(request):
 	return render(request, 'IFB299app/dashboard.html')
 
+
+
+class UserFormView(View):
+form_class = UserForm
+template_name = 'registration.html'
+
+def get(self, request):
+form = self.form_class(none)
+return render(request, self.template_name, {'form': form})
+
+def post (self, request):
+form = self.form_class(request.POST)
+
+if form.is_valid();
+
+user = form.save(commit = False)
+firstname = form.cleaned_data['firstname']
+lastname = form.cleaned_data['lastname']
+email = form.cleaned_data['email']
+password = form.cleaned_data['password']
+
+user.set_password(password)
+user.save()
+
+user = authenticate(email = email, password = password)
+
+
+if user is not None:
+if user.is_active:
+login(request, user)
+return redirect('')# dashboard
+
+return render(request, self.template_name, {'form': form})
