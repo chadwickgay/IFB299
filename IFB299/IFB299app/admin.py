ccd5409a2b28f1ea81bde3a9e265c318871e7d09
Unknown <chadwick.gay@connect.qut.edu.au>
2017-09-25 09:01:23 +1000

    Column display for admin interface of additional fields

--- IFB299/IFB299app/admin.py
+++ IFB299/IFB299app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import City, Region, Country, Category, Location
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'region_id', 'country_id', 'latitude', 'longitude')
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'country_id')
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'latitude', 'longitude', 'city_id')
 
 # Register your models here.
+
+admin.site.register(City, CityAdmin)
+admin.site.register(Region, RegionAdmin)
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Location, LocationAdmin)
