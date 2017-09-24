08ebe68787f3862f500d20c03a05e32429c5249e
Unknown <chadwick.gay@connect.qut.edu.au>
2017-09-25 09:04:58 +1000

    View to process user registration

--- IFB299/IFB299app/views.py
+++ IFB299/IFB299app/views.py
@@ -1,21 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from forms import RegisterForm
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 
 # Create your views here.
-
-from django.http import HttpResponse
-
 def index(request):
 	return render(request, 'IFB299app/index.html')
 
 def createaccount(request):
 	return render(request, 'IFB299app/createaccount.html')
 
-def login(request):
+def login_view(request):
 	return render(request, 'IFB299app/login.html')
-
-def register(request):
-	return render(request, 'IFB299app/register.html')
 
 def dashboard(request):
 	return render(request, 'IFB299app/dashboard.html')
+
+def location(request):
+    return render(request, 'IFB299app/location.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/IFB299app/dashboard/')
+    else:
+        form = RegisterForm()
+    return render(request, 'IFB299app/register.html', {'form': form})
 
